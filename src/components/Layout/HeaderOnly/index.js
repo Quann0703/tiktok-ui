6d454d6b470a46c5b48c757c0eaff392bc1cb68f
Quann0703 <tranquan07112003@gmail.com>
2024-02-16 10:39:30 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,14 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './HeaderOnly.module.scss';
 import Header from '~/components/Layout/components/Header';
-
-function DefaultLayout({ children }) {
+const cx = classNames.bind(styles);
+function HeaderOnly({ children }) {
     return (
         <div>
             <Header />
-            <div className="container">
-                <div className="content">{children}</div>
+            <div className={cx('container')}>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
 }
 
-export default DefaultLayout;
+export default HeaderOnly;
